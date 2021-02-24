2b0fb237ae04f73099c03e45ab0acbfbacf1fe62
franciscojgsalguero1 <65239673+franciscojgsalguero1@users.noreply.github.com>
2021-02-24 21:29:19 +0100

    Entrega Examen
    Entrega Examen

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,11 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('examen');
 });
+
+Route::get('/tutores', 'TutoresController@create');
+Route::resource('/tutores/create', TutoresController::class)->parameters(['tutores' => 'tutor'])->names(['create' => 'tutores.crear']);
